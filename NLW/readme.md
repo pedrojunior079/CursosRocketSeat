a4d770228fdca027ae1abe3aa97ba40cc16f36d0
Pedro Arns Junior <pedroarnsjr@outlook.com>
2025-02-19 16:27:33 -0300

    Aula 01 - Parte 1

---
+++ NLW/readme.md
@@ -0,0 +1,3 @@
+npx create-next-app@latest --empty
+npm install lucide-react
+continua em 44:50min
---
+++ NLW/web/src/app/layout.tsx
@@ -0,0 +1,33 @@
+import './globals.css';
+
+import type { Metadata } from "next";
+import {Montserrat, Oxanium} from 'next/font/google'
+
+export const metadata: Metadata = {
+  title: "devstage",
+};
+
+const oxanium = Oxanium({
+  weight: ['500', '600'],
+  subsets: ['latin'],
+  variable: '--fomt-oxanium',
+})
+
+const montserrat = Montserrat({
+  weight: ['500', '600'],
+  subsets: ['latin'],
+  variable: '--font-montserrat', 
+})
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${oxanium.variable} ${montserrat.variable}`}>
+      <body className='bg-gray-900 text-gray-100 antialised'>{children}</body>
+    </html>
+  );
+}
